c79c113ed67b4f974bd8544de373b07e0f2d9c0b
rahulaes17 <rahulaes1704@gmail.com>
2026-04-13 20:46:45 +0530

    Connectiing MongoDB to Express

---
+++ Connection/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const app = express()
+const port = 3000
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'mySchool';
+
+app.get('/', async (req, res) => {
+
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('students');
+    const studentsData = await collection.find().toArray();
+    console.log(studentsData);
+
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
